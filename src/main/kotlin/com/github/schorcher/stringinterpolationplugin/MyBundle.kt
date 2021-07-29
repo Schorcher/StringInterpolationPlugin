1239bb51741896366406f16b5857e79d9643bc9b
GitHub Action <action@github.com>
2021-07-29 00:44:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/schorcher/stringinterpolationplugin/MyBundle.kt
+++ src/main/kotlin/com/github/schorcher/stringinterpolationplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.schorcher.stringinterpolationplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/schorcher/stringinterpolationplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/schorcher/stringinterpolationplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.schorcher.stringinterpolationplugin.services
 
+import com.github.schorcher.stringinterpolationplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
